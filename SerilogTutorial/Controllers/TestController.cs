9e7a39e7a95fe29cf8bd1de60831774ff1289fa6
Chinonso Ikewelugo <chinonso.ikewelugo@pwcng-rpa.com>
2024-04-06 13:52:47 +0100

    Add project files.

---
+++ SerilogTutorial/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace SerilogTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("test-logger")]
+        public ActionResult TestLogger()
+        {
+            Log.Information("This log is from the global Log class");
+            _logger.LogInformation("This log is from the injected ILogger");
+            return Ok();
+        }
+    }
+}
